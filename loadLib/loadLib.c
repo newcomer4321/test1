fd9e4eeb0588614dacdfcc2eb3ca58980a5d1028
newcomer4321 <193.483144@qq.com>
2018-09-10 07:02:07 -0700

    now we can load lib with loadLib.c

--- loadLib/loadLib.c
+++ loadLib/loadLib.c
@@ -7,6 +7,8 @@
 
 #define addReplyErrorFormat(c,tmp,args...) fprintf(stdout,tmp,##args)
 
+typedef int (*loadLibFunc)();
+
 int fileExists(char *name)
 {
     struct stat st;
@@ -18,9 +20,54 @@ int fileExists(char *name)
         
 }
 
+int loadLibCommand(char *path, char *name)
+{
+	if(path == NULL || name == NULL) {
+        addReplyErrorFormat(c,"lib's path or name can't be NULL\n");
+        return -1;	
+	}
+    
+    int res = 0;
+    int pathLen = strlen(path);
+    int nameLen = strlen(name);
+
+    char *filename = NULL;
+
+    if(path[pathLen - 1] == '/') {
+        filename = (char *)malloc(sizeof(char)*(pathLen + nameLen - 1));
+        sprintf(filename, "%s%s", path, name);
+    } else {
+        filename = (char *)malloc(sizeof(char)*(pathLen + nameLen));
+        sprintf(filename, "%s/%s", path, name);
+    }
+
+    if(fileExists(filename)) {
+        void *handle = dlopen(filename, RTLD_GLOBAL | RTLD_NOW);
+
+        if(handle == NULL) {
+            addReplyErrorFormat(c,"%s\n",dlerror());
+            res = -1;
+        } else {
+            loadLibFunc loadInit = dlsym(handle, "loadInit");
+
+            if(loadInit == NULL) {
+                addReplyErrorFormat(c,"lib must have func  loadInit() and return int\n");
+                res = -1;
+            } else {
+                loadInit();
+            }
+            close(handle);
+        }    
+    }
+    return res;
+}
+
 int main(int argc, char **argv)
 {
     if(argc != 3) {
         addReplyErrorFormat(c,"./func path name.so\n");
     }
+
+    int res = loadLibCommand(argv[1], argv[2]);
+    return res;
 }
